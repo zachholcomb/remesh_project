d54bf471e67d1b838280f7dcfb8141a548fb92ce
Zach Holcomb <zachholcombmusic@gmail.com>
2021-02-03 08:54:42 -0700

    Adds seeds for manual testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_bot_rails'
+
+Thought.destroy_all
+Message.destroy_all
+Conversation.destroy_all
+
+# CONVERSATIONS
+conversations = FactoryBot.create_list(:conversation, 5, start_date: Date.today)
+
+# MESSAGES
+conversations.each do |convo|
+  FactoryBot.create_list(:message, 5, conversation: convo)
+end
+
+# THOUGHTS
+messages = Message.all
+messages.each do |message|
+  FactoryBot.create_list(:thought, 5, message: message)
+end
