7ad8ffc2b2d8e76872b9b2a76926b67cdecdb6c6
Zach Holcomb <zachholcombmusic@gmail.com>
2021-02-02 20:13:22 -0700

    Sets up model relationships

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,6 @@
 class Conversation < ApplicationRecord
+  has_many :messages, dependent: :destroy
+  has_many :thoughts, through: :messages, dependent: :destroy
   
+  validates :title, :start_date, presence: true
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ApplicationRecord
-  
+  belongs_to :conversation
+  has_many :thoughts
+  validates :text, presence: true
 end
