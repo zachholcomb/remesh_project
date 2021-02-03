71c6ea7686baec06987243c56dfd540a52857aa2
Zach Holcomb <zachholcombmusic@gmail.com>
2021-02-02 19:55:15 -0700

    Adds message table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_025137) do
+ActiveRecord::Schema.define(version: 2021_02_03_025305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,4 +20,13 @@ ActiveRecord::Schema.define(version: 2021_02_03_025137) do
     t.date "start_date"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "text"
+    t.bigint "conversation_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+  end
+
+  add_foreign_key "messages", "conversations"
 end
