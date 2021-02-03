65adf6bf70fa3f20caabad2faadbe5c1d9003a76
Zach Holcomb <zachholcombmusic@gmail.com>
2021-02-03 08:54:31 -0700

    Adds search model method to convo

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,13 @@
 class Conversation < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :thoughts, through: :messages, dependent: :destroy
-  
+
   validates :title, :start_date, presence: true
+
+  class << self
+    def search(params)
+      cleaned_query = "%#{params[:search]}%"
+      Conversation.where("title LIKE ?", cleaned_query)
+    end
+  end
 end
