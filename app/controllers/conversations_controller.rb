d027fb380bca117b11b20a5eebe3114aef03b96f
Zach Holcomb <zachholcombmusic@gmail.com>
2021-02-02 21:17:27 -0700

    Adds conversations index, show routing

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,9 @@
+class ConversationsController < ApplicationController
+  def index
+    @conversations = Conversation.all
+  end
+  
+  def show
+    @conversation = Conversation.find(params[:id])
+  end
+end
